f453858a21cfd12df9a37311202322b7e43fa9e2
ziuus <noelyt101@gmail.com>
2026-09-25 21:55:15 +0530

    chore: clean up scratch files and backup junk

--- scratch_http.rs
+++
@@ -1,7 +0,0 @@
-use extism_pdk::*;
-#[plugin_fn]
-pub fn fetch() -> FnResult<String> {
-    let req = HttpRequest::new("https://api.coincap.io/v2/assets/bitcoin");
-    let res = http::request::<()>(&req, None)?;
-    Ok(String::from_utf8(res.body())?)
-}
